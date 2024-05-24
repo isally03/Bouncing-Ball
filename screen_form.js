d6968a23e909587cac4c223aec2ea1bd4a8aa1ae
isally03 <isally03@naver.com>
2024-05-24 23:49:04 +0900

    화면 디자인

--- screen_form.js
+++ screen_form.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         finalScoreStr.split('').forEach((digit, index) => {
             setTimeout(() => {
                 animateDigit(`#digit${index + 1}`, digit);
-            }, index * 500); // Delay each animation start by 500ms
+            }, index * 500);
         });
     }
 
@@ -18,7 +18,7 @@ $(document).ready(function() {
                 numbers += Math.floor(Math.random()*10) + '<br>';
             }
         }
-        numbers += finalDigit + '<br>'; // Ensure the final digit is the last number
+        numbers += finalDigit + '<br>'; 
 
         for (var i = 0; i < 10; i++) {
             for (var j = 0; j < 10; j++) {
@@ -26,17 +26,11 @@ $(document).ready(function() {
             }
         }
         $digit.html('<span>' + numbers + '</span>');
-        
-        // Start the animation
         $digit.find('span').css('animation', 'slotSpin 2s linear infinite');
-        
-        // Stop the animation after a few seconds and display the final digit with a slowing effect
         setTimeout(function() {
             $digit.find('span').css('animation', 'slowStop 1s cubic-bezier(0.25, 0.1, 0.25, 1.0) forwards');
-            // setTimeout( 1000); // Duration of the slow stop animation
-        }, 2000); // Duration of the main animation before stopping
+            
+        }, 2000); 
     }
-
-    // Example usage: Start the animation with a final score of 3654
     startPachinkoAnimation(3654);
 });
