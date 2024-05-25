3195b1e80d653ee8fc94f9fc0f22fe9df2c0c058
kjs1214 <163082352+kjs1214@users.noreply.github.com>
2024-05-25 11:57:04 +0900

    환경설정 쪽 수정완료 초기값 설정까지 수정완료

--- screen_form.js
+++ screen_form.js
@@ -5,70 +5,9 @@ $(document).ready(function() {
     var gameoverMusic = new Audio("gameover1.wav");
     var backgroundMusicVolume;
     var gameoverMusicVolume;
-    function startSlotAnimation(finalScore) {
-        var finalScoreStr = finalScore.toString().padStart(4, '0');
-
-        finalScoreStr.split('').forEach((digit, index) => {
-            setTimeout(() => {
-                animateDigit(`#digit${index + 1}`, digit);
-            }, index * 500);
-        });
-    }
-
-    function animateDigit(selector, finalDigit) {
-        var $digit = $(selector);
-        var numbers = '';
-
-        for (var i = 0; i < 10; i++) {
-            for (var j = 0; j < 10; j++) {
-                numbers += Math.floor(Math.random()*10) + '<br>';
-            }
-        }
-        numbers += finalDigit + '<br>'; 
-
-        for (var i = 0; i < 10; i++) {
-            for (var j = 0; j < 10; j++) {
-                numbers += Math.floor(Math.random()*10) + '<br>';
-            }
-        }
-        $digit.html('<span>' + numbers + '</span>');
-        $digit.find('span').css('animation', 'slotSpin 2s linear infinite');
-        setTimeout(function() {
-            $digit.find('span').css('animation', 'slowStop 1s cubic-bezier(0.25, 0.1, 0.25, 1.0) forwards');
-            
-        }, 2000); 
-    }
-
-    var ballColor = "black"; 
-    var backImage = "background1.jpg";
-    var backgroundMusic = new Audio("backgroundmusic1.wav");
-    backgroundMusic.loop = true;
-    var gameoverMusic = new Audio("gameover1.wav");
-
-    function settings() {
-        $("#main_page").hide();
-        $("#customize_page").show();
-    }
-
-    function settingsSave() {
-        ballColor = $("input[name='ballColor']:checked").val();
-        backImage = $("input[name='backColor']:checked").val();
-        var musicSrc = $("input[name='music']:checked").val();
-        var overMusicSrc = $("input[name='overMusic']:checked").val();
-
-        backgroundMusic.src = musicSrc;
-        gameoverMusic.src = overMusicSrc;
-
-        $("#customize_page").hide();
-        $("#main_page").show();
-    }
-
-    function settingsCancel() {
-        $("#customize_page").hide();
-        $("#main_page").show();
-    }
-
-    $("#customize").on("click", function() {
+    backgroundMusic.volume=0.5;
+    gameoverMusic.volume=0.5;
+     $("#customize").on("click", function() {
         settings();
     });
 
@@ -118,4 +57,61 @@ $(document).ready(function() {
             $("#overVolumeValue").text(50);
         }
     });
+    function startSlotAnimation(finalScore) {
+        var finalScoreStr = finalScore.toString().padStart(4, '0');
+
+        finalScoreStr.split('').forEach((digit, index) => {
+            setTimeout(() => {
+                animateDigit(`#digit${index + 1}`, digit);
+            }, index * 500);
+        });
+    }
+
+    function animateDigit(selector, finalDigit) {
+        var $digit = $(selector);
+        var numbers = '';
+
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                numbers += Math.floor(Math.random()*10) + '<br>';
+            }
+        }
+        numbers += finalDigit + '<br>'; 
+
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                numbers += Math.floor(Math.random()*10) + '<br>';
+            }
+        }
+        $digit.html('<span>' + numbers + '</span>');
+        $digit.find('span').css('animation', 'slotSpin 2s linear infinite');
+        setTimeout(function() {
+            $digit.find('span').css('animation', 'slowStop 1s cubic-bezier(0.25, 0.1, 0.25, 1.0) forwards');
+            
+        }, 2000); 
+    }
+
+
+
+    function settings() {
+        $("#main_page").hide();
+        $("#customize_page").show();
+    }
+
+    function settingsSave() {
+        ballColor = $("input[name='ballColor']:checked").val();
+        backImage = $("input[name='backImage']:checked").val();
+        var musicSrc = $("input[name='music']:checked").val();
+        var overMusicSrc = $("input[name='overMusic']:checked").val();
+
+        backgroundMusic.src = musicSrc;
+        gameoverMusic.src = overMusicSrc;
+        $("#customize_page").hide();
+        $("#main_page").show();
+    }
+
+    function settingsCancel() {
+        $("#customize_page").hide();
+        $("#main_page").show();
+    }
 });
