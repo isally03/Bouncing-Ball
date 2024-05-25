1810f81d1fee91627682acdb378398bf23c7e463
dlstkdals123 <dlstkdals123@naver.com>
2024-05-25 19:38:34 +0900

    tmp

--- screen_form.js
+++ screen_form.js
@@ -8,53 +8,53 @@ var gameoverMusicVolume = 0.5;
 
 var storybox;
 
-$(document).ready(function () {
+$(document).ready(function() {
     storybox = $(".storybox");
 
-    $("#customize").on("click", function () {
+    $("#customize").on("click", function() {
         settings();
     });
 
-    $("#return_img1").on("click", function () {
+    $("#return_img1").on("click", function() {
         settingsCancel();
     });
-    $("#return_img2").on("click", function () {
+     $("#return_img2").on("click", function() {
         settingsCancel();
     });
 
-    $("#musicVolume").on("input", function () {
+    $("#musicVolume").on("input", function() {
         $("#musicVolumeValue").text($(this).val());
         backgroundMusic.volume = $(this).val() / 100;
     });
 
-    $("#overVolume").on("input", function () {
+    $("#overVolume").on("input", function() {
         $("#overVolumeValue").text($(this).val());
         gameoverMusic.volume = $(this).val() / 100;
 
-        $("#Muteall").change(function () {
-            if ($(this).is(":checked")) {
-                backgroundMusic.volume = 0;
-                $("#musicVolume").val(0).prop("disabled", true);
-                $("#musicVolumeValue").text(0);
-                gameoverMusic.volume = 0;
-                $("#overVolume").val(0).prop("disabled", true);
-                $("#overVolumeValue").text(0);
-            }
-            else {
-                var volume = 50;
-                backgroundMusic.volume = volume / 100;
-                $("#musicVolume").val(50).prop("disabled", false);
-                $("#musicVolumeValue").text(50);
-                gameoverMusic.volume = volume / 100;
-                $("#overVolume").val(50).prop("disabled", false);
-                $("#overVolumeValue").text(50);
-            }
-        });
+    $("#Muteall").change(function(){
+        if($(this).is(":checked")){
+           backgroundMusic.volume=0;
+           $("#musicVolume").val(0).prop("disabled",true);
+           $("#musicVolumeValue").text(0);
+           gameoverMusic.volume=0;
+           $("#overVolume").val(0).prop("disabled",true);
+           $("#overVolumeValue").text(0);
+        }
+        else{
+            var volume=50;
+            backgroundMusic.volume=volume/100;
+            $("#musicVolume").val(50).prop("disabled",false);
+            $("#musicVolumeValue").text(50);
+            gameoverMusic.volume=volume/100;
+            $("#overVolume").val(50).prop("disabled",false);
+            $("#overVolumeValue").text(50);
+        }
     });
+});
 
     $("#startGame").on("click", prolog);
     $("#challenge").on("click", challenge);
-    $("#exit").on("click", exit);
+    $("#exit").on("click", exit);  
     $("#exit_img").on("click", finishStory);
     $(document).on("mousemove", mouseMoveSpeed);
 });
@@ -75,22 +75,22 @@ function animateDigit(selector, finalDigit) {
 
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
-            numbers += Math.floor(Math.random() * 10) + '<br>';
+            numbers += Math.floor(Math.random()*10) + '<br>';
         }
     }
-    numbers += finalDigit + '<br>';
+    numbers += finalDigit + '<br>'; 
 
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
-            numbers += Math.floor(Math.random() * 10) + '<br>';
+            numbers += Math.floor(Math.random()*10) + '<br>';
         }
     }
     $digit.html('<span>' + numbers + '</span>');
     $digit.find('span').css('animation', 'slotSpin 1s linear infinite');
-    setTimeout(function () {
+    setTimeout(function() {
         $digit.find('span').css('animation', 'slowStop 1s cubic-bezier(0.25, 0.1, 0.25, 1.0) forwards');
-
-    }, 1000);
+        
+    }, 1000); 
 }
 
 function settings() {
@@ -116,7 +116,7 @@ function settingsCancel() {
     $("#main_page").show();
 }
 
-function prolog() {
+function prolog(){
     $("#main_page").hide();
     $("#storyboard").show();
     $(window).keydown(playStory);
@@ -125,32 +125,32 @@ function prolog() {
 
 var index = 0;
 
-function playStory() {
-    if ((index) == storybox.length) {
+function playStory(){
+    if((index)== storybox.length ){
         finishStory();
     }
-    else {
-        console.log("index: " + index + "\nstorybox[index]" + storybox[index]);
+    else{
+        console.log("index: "+index+"\nstorybox[index]"+ storybox[index]);
         storybox.eq(index).css("display", "flex");
         index++;
     }
-
+   
 }
 
-function finishStory() {
+function finishStory(){
     $(window).off();
     $("#storyboard").hide();
     gameStart();
 }
 
 
-function showResult() {
+function showResult(){
     $("#main_menu").show();
     $("#result_page").show();
     startSlotAnimation(score);
 }
 
-function challenge() {
+function challenge(){
     $("#main_page").hide();
     $("#challenge_page").show();
 }
